870f022b62dbbd661e3e003b26167f12cef883d2
Tulip Pink <pinktulips@hotmail.com>
2019-01-13 18:07:16 -0800

    start game logic

--- Lab04_TicTacToe/Lab04_TicTacToe/Classes/Game.cs
+++ Lab04_TicTacToe/Lab04_TicTacToe/Classes/Game.cs
@@ -30,8 +30,11 @@ namespace Lab04_TicTacToe.Classes
 		/// <returns>Winner</returns>
 		public Player Play()
 		{
+            PlayerOne.Marker = "X";
+            PlayerOne.IsTurn = true;
 
-			//TODO: Complete this method and utilize the rest of the class structure to play the game.
+
+            //TODO: Complete this method and utilize the rest of the class structure to play the game.
 
             /*
              * Complete this method by constructing the logic for the actual playing of Tic Tac Toe. 
@@ -47,7 +50,7 @@ namespace Lab04_TicTacToe.Classes
 
             Use any and all pre-existing methods in this program to help construct the method logic. 
              */
-		}
+        }
 
 
 		/// <summary>
