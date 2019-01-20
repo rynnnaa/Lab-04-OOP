39b1ee090c9531095e327a40567ce70294ab73ca
Tulip Pink <pinktulips@hotmail.com>
2019-01-19 19:15:15 -0800

    done

--- Lab04_TicTacToe/XUnitTestProject1/UnitTest1.cs
+++ Lab04_TicTacToe/XUnitTestProject1/UnitTest1.cs
@@ -26,5 +26,69 @@ namespace Lab40104xUnit
             Assert.True(game.CheckForWinner(game.Board));
         }
 
+        [Fact]
+        public void CanSwitchPlayers()
+        {
+            //arrange
+            Player playerOne = new Player();
+            playerOne.Marker = "X";
+
+            Player playerTwo = new Player();
+            playerTwo.Marker = "O";
+
+            //act
+            Game game = new Game(playerOne, playerTwo);
+
+            //assert
+            game.SwitchPlayer();
+            Assert.True(game.PlayerOne.IsTurn);
+
+            game.SwitchPlayer();
+            Assert.True(game.PlayerTwo.IsTurn);
+        }
+
+        [Fact]
+        public void CorrectIndexWork()
+        {
+            //arrange
+            Player playerOne = new Player();
+            playerOne.Marker = "X";
+
+            Player playerTwo = new Player();
+            playerTwo.Marker = "O";
+
+            //act
+            Game game = new Game(playerOne, playerTwo);
+
+            Position positionOne = Player.PositionForNumber(1);
+            Position positionTwo = Player.PositionForNumber(2);
+
+            game.Board.GameBoard[positionOne.Row, positionOne.Column] = playerOne.Marker;
+            game.Board.GameBoard[positionTwo.Row, positionTwo.Column] = playerTwo.Marker;
+
+            //assert
+            Assert.Equal("X", game.Board.GameBoard[0, 0]);
+            Assert.Equal("O", game.Board.GameBoard[0, 1]);
+        }
+
+        [Fact]
+        public void WinnerXWorks()
+        {
+            //arrange
+            Player playerOne = new Player();
+            playerOne.Marker = "X";
+            Player playerTwo = new Player();
+            playerTwo.Marker = "O";
+
+            //act
+            Game game = new Game(playerOne, playerTwo);
+
+            game.Board.GameBoard[0, 0] = "X";
+            game.Board.GameBoard[1, 0] = "X";
+            game.Board.GameBoard[2, 0] = "X";
+
+            //assert
+            Assert.True(game.CheckForWinner(game.Board));
+        }
     }
 }
