36a3302e4288f87177c08a5a6352f419500c461d
Tulip Pink <pinktulips@hotmail.com>
2019-01-19 19:09:45 -0800

    unit testing for game

--- Lab04_TicTacToe/XUnitTestProject1/UnitTest1.cs
+++ Lab04_TicTacToe/XUnitTestProject1/UnitTest1.cs
@@ -1,14 +1,30 @@
-using System;
+using Lab04_TicTacToe.Classes;
+using Lab04_TicTacToe.Classes.Lab04_TicTacToe.Classes;
 using Xunit;
 
-namespace XUnitTestProject1
+namespace Lab40104xUnit
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void WinnerOWorks()
         {
+            //arrange
+            Player playerOne = new Player();
+            playerOne.Marker = "X";
+            Player playerTwo = new Player();
+            playerTwo.Marker = "O";
 
+            //act
+            Game game = new Game(playerOne, playerTwo);
+
+            game.Board.GameBoard[0, 0] = "O";
+            game.Board.GameBoard[1, 0] = "O";
+            game.Board.GameBoard[2, 0] = "O";
+
+            //assert
+            Assert.True(game.CheckForWinner(game.Board));
         }
+
     }
 }
